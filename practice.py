c044206e3bda856cc627f5bbfa903f198652df04
smailenn <mailensean@gmail.com>
2020-04-28 23:36:47 -0500

    lots of great work!  getting data and plots

--- practice.py
+++ practice.py
@@ -1,13 +1,38 @@
-import pandas
+#import libraries
+import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
+#Calibration variables and Accelerometer setup
+
+
+
 #read the data into an array from text file
-wax = pandas.read_csv('C:\\Users\\smailen\\OneDrive - Quality Bicycle Products\\Vibration Analysis\\Waxwing Test\\WAXWING00.txt', sep='\t')
-#with open('C:\\Users\\smailen\\OneDrive - Quality Bicycle Products\\Vibration Analysis\\Waxwing Test\\WAXWING00.txt', mode='r') as vibration: 
-    #WAXWING00 = csv.reader(vibration, delimiter='\t')  
-print(wax)
+dataframe = pd.read_csv('C:\\Users\\smailen\\OneDrive - Quality Bicycle Products\\Vibration Analysis\\Waxwing Test\\WAXWING00.txt', sep='\t')
 
-#Waxwing = open('C:\\Users\\smailen\\OneDrive - Quality Bicycle Products\\Vibration Analysis\\Waxwing Test\\Waxwing00.txt')
+#get info about the array
+#type(dataframe)
+#dataframe.shape
+#dataframe.info()
+#print(dataframe)
+
+#export sheet to Excel for record and review
+#dataframe.to_excel('WAXWING00.xlsx', sheet_name='Waxwing00')
+
+#Seperate data into column arrays
+time = dataframe["Time"]
+Sensor1xRaw = dataframe["Sensor 1 X"]
+Sensor1yRaw = dataframe["Sensor 1 Y"]
+Sensor1zRaw = dataframe["Sensor 1 Z"]
+Sensor2xRaw = dataframe["Sensor 2 X"]
+Sensor2yRaw = dataframe["Sensor 2 Y"]
+Sensor2zRaw = dataframe["Sensor 2 Z"]
+
+#Calibrate, orientate, and Normalize readings to g's
 
 
+#make some plots
+plt.plot(time, Sensor2y, time, Sensor1y)
+plt.show()
+
+plt.plot()
