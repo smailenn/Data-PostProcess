9aaff1760d58da8d940eea343e6553957f38d76f
smailenn <mailensean@gmail.com>
2020-05-08 00:24:44 -0500

    better RMS, working on PSD

--- Main.py
+++ Main.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 import math 
 from numpy import savetxt
 from scipy import stats
+from scipy import signal
 
 #Calibration variables and Accelerometer setup
 #Sensor 1 - upper sensor, at Handlebar
@@ -17,7 +18,7 @@ accel_raw_scale_pos = 32768 #raw value range positive values
 AS = 'y' # Accel Set 
 
 #read the data into an array from text file
-dataframe = pd.read_csv('C:\\Users\\smailen\\OneDrive - Quality Bicycle Products\\Vibration Analysis\\Waxwing Test\\WAXWING01.txt', sep='\t')
+dataframe = pd.read_csv('C:\\Users\\smailen\\OneDrive - Quality Bicycle Products\\Vibration Analysis\\Waxwing Test\\WAXWING06.txt', sep='\t')
 #dataframe = pd.read_csv('C:\\Users\\smailen\\OneDrive - Quality Bicycle Products\\Vibration Analysis\\Warbird V3\\WARBIRD02.txt', sep='\t')
 #dataframe = pd.read_excel('C:\\Users\\smailen\\OneDrive - Quality Bicycle Products\\Vibration Analysis\\Cutthroat V2\\Cutthroat Vibration Data Vert.xlsx')
 
@@ -36,15 +37,14 @@ Sensor2xRaw = dataframe["Sensor 2 X"]
 Sensor2yRaw = dataframe["Sensor 2 Y"]
 Sensor2zRaw = dataframe["Sensor 2 Z"]
 
-
 #Time and Index review
 time = time - time[0]  #Normalize time back to starting at 0
 time = time/1000000 #convert uS to S
-time_int = np.ediff1d(time)  # Find time intervals (time between measurements)
-time_int_avg = stats.trim_mean(time_int,0.1) # Trim time intervals of SD lag points, large time intervals during a SD write 
-print('Time Interval Average =', time_int_avg,'S')
+time_int = np.ediff1d(time)  # Find time intervals - this is array (time between measurements)
+time_int_avg = stats.trim_mean(time_int,0.1) # Trim time intervals of SD lag points, large time intervals during a SD write, this is scalar
+#print('Time Interval Average =', "%.8f"% time_int_avg,'S')
 #savetxt('time_int.csv', time_int, delimiter=',')  # save Time Interval Array as CSV for review if needed
-print(len(time))
+#print(len(time))
 
 #Calibrate, orientate, and Normalize readings to G's
 #Calculate offset values from calibration test at beginning of test runs, ideally done at same time as testing
@@ -64,7 +64,8 @@ Sensor2x_g = (Sensor2xRaw + Sensor2x_offset)*(accel_range/accel_raw_scale_pos)
 Sensor2y_g = ((Sensor2yRaw + Sensor2y_offset)*(accel_range/accel_raw_scale_pos))+1
 Sensor2z_g = ((Sensor2zRaw + Sensor2z_offset)*(accel_range/accel_raw_scale_pos))*Calibrate_direction
 
-#If needed trim the arrays for bad/false/fault data
+#If needed trim the arrays for bad/false/faulty data
+#Trimming is based on # of samples, not in time array
 trim_beg = 0 #trim beginning of array
 trim_end = 300000  #trim end of array
 time = time[trim_beg:trim_end]
@@ -75,7 +76,7 @@ Sensor2x_g = Sensor2x_g[trim_beg:trim_end]
 Sensor2y_g = Sensor2y_g[trim_beg:trim_end]
 Sensor2z_g = Sensor2z_g[trim_beg:trim_end]
 trim_size = trim_end - trim_beg
-print('Size of array after trim =', trim_size)
+print('Size of array after trim =', trim_size,'samples')
 
 #Analyze the Arrays ============================================================================================================
 # Find Average of sensor data
@@ -85,12 +86,12 @@ S1xmean = np.mean(Sensor1x_g)
 S2xmean = np.mean(Sensor2x_g)    
 S1zmean = np.mean(Sensor1z_g)
 S2zmean = np.mean(Sensor2z_g)  
-print('Sensor 1 Y Axis Average = ', S1ymean)
-print('Sensor 2 Y Axis Average = ', S2ymean)
-print('Sensor 1 X Axis Average = ', S1xmean)
-print('Sensor 2 X Axis Average = ', S2xmean)
-print('Sensor 1 Z Axis Average = ', S1zmean)
-print('Sensor 2 Z Axis Average = ', S2zmean)
+print('Sensor 1 Y Axis Average = ', '%.5f'% S1ymean, 'G')
+print('Sensor 2 Y Axis Average = ', '%.5f'% S2ymean, 'G')
+print('Sensor 1 X Axis Average = ', '%.5f'% S1xmean, 'G')
+print('Sensor 2 X Axis Average = ', '%.5f'% S2xmean, 'G')
+print('Sensor 1 Z Axis Average = ', '%.5f'% S1zmean, 'G')
+print('Sensor 2 Z Axis Average = ', '%.5f'% S2zmean, 'G')
 
 # Find MAX of sensor data
 S1yMAX = np.amax(Sensor1y_g)
@@ -99,12 +100,12 @@ S1xMAX = np.amax(Sensor1x_g)
 S2xMAX = np.amax(Sensor2x_g)    
 S1zMAX = np.amax(Sensor1z_g)
 S2zMAX = np.amax(Sensor2z_g)    
-print('Sensor 1 Y Axis MAX = ', S1yMAX)
-print('Sensor 2 Y Axis MAX = ', S2yMAX)
-print('Sensor 1 X Axis MAX = ', S1xMAX)
-print('Sensor 2 X Axis MAX = ', S2xMAX)
-print('Sensor 1 Z Axis MAX = ', S1zMAX)
-print('Sensor 2 Z Axis MAX = ', S2zMAX)
+print('Sensor 1 Y Axis MAX = ', '%.5f'% S1yMAX, 'G')
+print('Sensor 2 Y Axis MAX = ', '%.5f'% S2yMAX, 'G')
+print('Sensor 1 X Axis MAX = ', '%.5f'% S1xMAX, 'G')
+print('Sensor 2 X Axis MAX = ', '%.5f'% S2xMAX, 'G')
+print('Sensor 1 Z Axis MAX = ', '%.5f'% S1zMAX, 'G')
+print('Sensor 2 Z Axis MAX = ', '%.5f'% S2zMAX, 'G')
 
 # Find MIN of sensor data
 S1yMIN = np.amin(Sensor1y_g)
@@ -113,39 +114,58 @@ S1xMIN = np.amin(Sensor1x_g)
 S2xMIN = np.amin(Sensor2x_g)    
 S1zMIN = np.amin(Sensor1z_g)
 S2zMIN = np.amin(Sensor2z_g)    
-print('Sensor 1 Y Axis MIN = ', S1yMIN)
-print('Sensor 2 Y Axis MIN = ', S2yMIN)
-print('Sensor 1 X Axis MIN = ', S1xMIN)
-print('Sensor 2 X Axis MIN = ', S2xMIN)
-print('Sensor 1 Z Axis MIN = ', S1zMIN)
-print('Sensor 2 Z Axis MIN = ', S2zMIN)
+print('Sensor 1 Y Axis MIN = ', '%.5f'% S1yMIN, 'G')
+print('Sensor 2 Y Axis MIN = ', '%.5f'% S2yMIN, 'G')
+print('Sensor 1 X Axis MIN = ', '%.5f'% S1xMIN, 'G')
+print('Sensor 2 X Axis MIN = ', '%.5f'% S2xMIN, 'G')
+print('Sensor 1 Z Axis MIN = ', '%.5f'% S1zMIN, 'G')
+print('Sensor 2 Z Axis MIN = ', '%.5f'% S2zMIN, 'G')
 
 # Transmissibility and Isolation %
-Trans_x = (S1xmean/S2xmean)*100
-Trans_y = (S1ymean/S2ymean)*100
-Trans_z = (S1zmean/S2zmean)*100
-print('X Axis Transmissibility =', Trans_x, '%')
-print('Y Axis Transmissibility =', Trans_y, '%')
-print('Z Axis Transmissibility =', Trans_z, '%')
+Trans_x = (S1xmean/S2xmean)
+Trans_y = (S1ymean/S2ymean)
+Trans_z = (S1zmean/S2zmean)
+print('X Axis Transmissibility =', '%.5f'%  Trans_x,)
+print('Y Axis Transmissibility =', '%.5f'%  Trans_y,)
+print('Z Axis Transmissibility =', '%.5f'%  Trans_z,)
+
+# Difference in arrays (vector)
+# Looking at difference by vector, then averaging 
+Xdiff = Sensor2x_g - Sensor1x_g
+Ydiff = Sensor2y_g - Sensor1y_g
+Zdiff = Sensor2z_g - Sensor1z_g
+Xdiff_avg = np.mean(Xdiff)
+Ydiff_avg = np.mean(Ydiff)
+Zdiff_avg = np.mean(Zdiff)
+#print('Avg diff. of X Axis =', '%.5f'% Xdiff_avg,'G')
+#print('Avg diff. of Y Axis =', '%.5f'% Ydiff_avg,'G')
+#print('Avg diff. of Z Axis =', '%.5f'% Zdiff_avg,'G')
+# looking at % difference of vectors
+Xpd = np.mean(Xdiff/Sensor2x_g)*100  
+Ypd = np.mean(Ydiff/Sensor2y_g)*100
+Zpd = np.mean(Zdiff/Sensor2z_g)*100
+#print('Avg %\ diff. of X Axis =', '%.5f'% Xpd,'G')
+#print('Avg %\ diff. of Y Axis =', '%.5f'% Ypd,'G')
+#print('Avg %\ diff. of Z Axis =', '%.5f'% Zpd,'G')
 
 #Additonal statistics
 S1yMedian = np.median(Sensor1y_g)
 S2yMedian = np.median(Sensor2y_g)
-print('Sensor 1 Y Axis Median =', S1yMedian)
-print('Sensor 2 Y Axis Median =', S2yMedian)
+#print('Sensor 1 Y Axis Median =', '%.5f'%  S1yMedian, 'G')
+#print('Sensor 2 Y Axis Median =', '%.5f'%  S2yMedian, 'G')
 S1yP2P = S1yMAX - S1yMIN
 S2yP2P = S2yMAX - S1yMIN
-print('Sensor 1 P2P =', S1yP2P)
-print('Sensor 2 P2P =', S2yP2P)
+print('Sensor 1 Total Range =', '%.5f'%  S1yP2P, 'G')
+print('Sensor 2 Total Range =', '%.5f'%  S2yP2P, 'G')
 S1yRMS = math.sqrt(np.sum(Sensor1y_g**2)/trim_size)
 S2yRMS = math.sqrt(np.sum(Sensor2y_g**2)/trim_size)
-print('Sensor 1 RMS =', S1yRMS)
-print('Sensor 2 RMS =', S2yRMS)
+print('Sensor 1 RMS =', '%.5f'%  S1yRMS, 'G')
+print('Sensor 2 RMS =', '%.5f'%  S2yRMS, 'G')
 S1vs2RMS = ((S2yRMS-S1yRMS)/S2yRMS)*100
-print(S1vs2RMS, '% Reduction RMS')
+print('%.3f'% S1vs2RMS,'% Reduction RMS')
 
 
-#make some plots
+#make some plots========================================================================================
 #Acceleration plots vs. time
 fig, (x1, y1, z1) = plt.subplots(3,1)  #Create one Figure with 3 plots
 fig.subplots_adjust(hspace=0.5) # extra space between plots
@@ -158,6 +178,7 @@ x1.set_title('Acceleration (X Axis)')
 x1.set_xlabel('Time(Sec)')
 x1.set_ylabel('G')
 x1.legend()
+x1.grid()
 
 y1 = plt.subplot(312)
 y1.plot(time, Sensor2y_g, label = 'Axle Sensor - Y', color='r')
@@ -167,6 +188,7 @@ y1.set_title('Acceleration (Y Axis)')
 y1.set_xlabel('Time(Sec)')
 y1.set_ylabel('G')
 y1.legend()
+z1.grid()
 
 z1 = plt.subplot(313)
 z1.plot(time, Sensor2z_g, label = 'Axle Sensor - Z', color='r')
@@ -176,54 +198,56 @@ z1.set_title('Acceleration (Z Axis)')
 z1.set_xlabel('Time(Sec)')
 z1.set_ylabel('G')
 z1.legend()
+z1.grid()
 
-#FFT plots
-# Set size of array for FFT, time and data arrays must equal
+#FFT========================================================================================
+plt.figure(2)
+# Set size of array for FFT (time and data arrays must equal)
 X = 1
 N = 0
 while N < trim_size:
-    N =2**X
+    N =2**X  #N must be a BASE-2 number less than total array size 
     X += 1
-    #print(N)
 X-= 2
 N =2**X # Size of array for FFT
-print('N=', N)
+print('FFT array size =', N)
 
 #FFT of Data
-#Fs = 1/time_int_avg
-Fs = 2406 # Sample Rate
+Fs = int(1/time_int_avg)
 print(Fs, 'Hz Sample Rate')
-T = (1/Fs) 
-print('T =', T)
-plt.figure(2)
-plt.plot(time, Sensor1y_g)
-
-plt.figure(3)
-print(')
-frequency = np.linspace(0.0, 1203, 131072)
-freq_data = np.fft.fft(Sensor1y_g[0:N])
-y = 2/N*np.abs(freq_data[0:131072])
-
-#xf = np.linspace(0.0, 1.0/(2.0*T), N/2)
-#xf = Fs/2
-#xf = np.linspace(0, 124, time)
-#yf = np.fft.fft(Sensor1y_g)
-#plt.plot(xf, 2.0/N * np.abs(yf[0:np.int(N/2)]))
-
-
-#ft = np.fft.fft(Sensor1y_g[0:N]) * time_int_avg  
-#freq = np.fft.fftfreq(N, time[0:N])
-#freq = freq[:N//2+1]
+print('N/2 =', int(N/2))
+frequency = np.linspace(0.0, int(Fs/2), int(N/2))  #X Axis Frequency range
+freq_data_1 = np.fft.fft(Sensor1y_g[0:N])  #FFT of Sensor[x]
+freq_data_2 = np.fft.fft(Sensor2y_g[0:N])  #FFT of Sensor[y]
+fft_1 = 2/N*np.abs(freq_data_1[0:int(N/2)]) # Take abs value of Sensor[x] 
+fft_2 = 2/N*np.abs(freq_data_2[0:int(N/2)]) # Take abs value of Sensor[y] 
 
 # plot results on an x-y scatter plot
-plt.plot(frequency, y)
-#fig, (fftx, ffty, ffty) = plt.subplots(3,1)
-#ffty = plt.subplot(312)
-#ffty.plot(freq, np.abs(ft)) # comma indicates a pixel marker
-#plt.xlabel('Frequency')
-#plt.ylabel('amplitude')
-#plt.xlim([0, 15])
+plt.plot(frequency, fft_2, label = 'Axle Sensor', color='r')
+plt.plot(frequency, fft_1, label = 'Handlebar Sensor')
+plt.title('FFT')
+plt.xlabel('Frequency (Hz)')
+plt.ylabel('Amplitude')
+plt.xlim([0, 400])
 #plt.ylim([0, 300])
+plt.legend()
+plt.grid()
 
+plt.figure(3)
+plt.hist(Sensor2y_g, bins='auto', label = "Handlebar Sensor", color = 'r')
+plt.hist(Sensor1y_g, bins='auto', label = 'Axle Sensor')
+plt.title('Y Axis Histogram')
+plt.xlabel('Acceleration (G)')
+plt.ylabel('Quantity')
+plt.xlim([-3,3])
+plt.legend()
+plt.grid()
 
+plt.figure(4)
+f, Pxx_den = signal.welch(Sensor1y_g, Fs)
+plt.semilogy(f, Pxx_den)
+#plt.ylim([0.5e-3, 1])
+plt.xlabel('frequency [Hz]')
+plt.ylabel('PSD [V**2/Hz]')
+plt.show()
 plt.show()
